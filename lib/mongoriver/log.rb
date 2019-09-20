1bad0aa0c36780c5118481b348c307df420508da
Rafal Boni <rafal@yesware.com>
2019-09-20 13:20:29 -0400

    Looks like creation with a log level doesn't quite work?

--- lib/mongoriver/log.rb
+++ lib/mongoriver/log.rb
@@ -1,7 +1,7 @@
 module Mongoriver
   module Logging
     def log
-      @@logger ||= Log4r::Logger.new("Stripe::Mongoriver", Log4r::ALL)
+      @@logger ||= Log4r::Logger.new("Stripe::Mongoriver")
     end
   end
 end
